a695243ae861f6c4d8d6a422b1b94decadd90f25
nishuldhakar <nishuldhakar.se@gmail.com>
2026-04-26 20:15:55 +0530

    updated ui

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -17,24 +17,15 @@ export default async function AuthLayout({
    }
    return (
       <main className="relative h-screen">
-         <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            preload="auto"
-            disableRemotePlayback
-            webkit-playsinline="true"
-            x5-playsinline="true"
-            poster="https://images.unsplash.com/photo-1557683316-973673baf926?w=1600&q=60"
-            className="absolute inset-0 w-full h-full object-cover pointer-events-none"
-         >
-            <source
-               src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260330_145725_08886141-ed95-4a8e-8d6d-b75eaadce638.mp4"
-               type="video/mp4"
-            />
-         </video>
-      
+               <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="none"
+        className="absolute inset-0 w-full h-full object-cover z-0 pointer-events-none"
+        src="https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260330_145725_08886141-ed95-4a8e-8d6d-b75eaadce638.mp4"
+      />
          <div className="relative h-full flex flex-col items-center justify-center">
             {children}
          </div>
